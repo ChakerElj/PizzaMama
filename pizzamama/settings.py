32c5d895d7398efe07e3ae6eed3c3399938d1ab5
Chaker <eljchaker232@gmail.com>
2022-03-14 14:01:38 +0100

    change

--- pizzamama/settings.py
+++ pizzamama/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-yju5vu2h3a9q+$vboi^!bpy*pt^itgrisa)x=rlw=7^r$am3ky'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['pizzamama484.herokuapp.com']
 
